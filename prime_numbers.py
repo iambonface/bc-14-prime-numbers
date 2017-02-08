939e56f8d37911001e2864d2da453edd9cc2e6a1
Bonface Mudiri <mudirib@gmail.com>
2017-02-08 23:07:28 +0300

    Rename target_list in get_prime() to target_prime for clarity

--- prime_numbers.py
+++ prime_numbers.py
@@ -7,7 +7,7 @@ Results:
     If the number is less than 1,  Nothing is returned
     Else, expect list of prime numbers
     example
-    get_prime(13) will print [2,3,5,7,11]
+    get_prime(13) will print [2,3,5,7,11,13]
 Exceptions:
     Raise TypeError if input n is of any other type other than int
 """
@@ -16,12 +16,11 @@ def get_prime(n):
 	if isinstance(n, int):
 		if n < 1:
 			return None
-		else:
-			target_list = range(2, n+1)
+		elif n >= 2:
+			target_prime = range(2, n+1)
 			for i in range(2,8):
-				"""iterate on all posible divisors and exclude multiples"""
-				target_list = [x for x in target_list if x == i or x % i ]
-			print(target_list)
+				target_prime = [x for x in target_prime if x == i or x % i ]
+			return target_prime
 
 	else:
 		raise TypeError("Invalid input {}".format(type(n)))
