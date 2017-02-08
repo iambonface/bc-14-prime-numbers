63384bc77dbcb255c31f64cce1e1a17c0f437ff6
Bonface Mudiri <mudirib@gmail.com>
2017-02-08 23:19:45 +0300

    Fix test_prime_numbers.py by changing from deprecated assertEquals to assertEqual

--- test_prime_numbers.py
+++ test_prime_numbers.py
@@ -8,18 +8,18 @@ class TestPrimeNumbers(unittest.TestCase):
 
 	def test_get_prime_input_13_returns_prime_list(self):
 		result = get_prime(13)
-		self.assertEquals(result, [2,3,5,7,11,13])
+		self.assertEqual(result, [2,3,5,7,11,13])
 
 	def test_get_prime_input_str_raises_typeerror(self):
 		self.assertRaises(TypeError, get_prime, "num")
 
 	def test_get_prime_output_has_2_as_first_element(self):
 		result = get_prime(7)
-		self.assertEquals(result[0], 2)
+		self.assertEqual(result[0], 2)
 
 	def test_get_prime_output_includes_input_if_input_is_prime(self):
 		result = get_prime(13)
-		self.assertEquals(result[-1], 13)
+		self.assertEqual(result[-1], 13)
 
 	def test_get_prime_output_has_no_even_number_except_2(self):
 		result = get_prime(13)
@@ -28,7 +28,7 @@ class TestPrimeNumbers(unittest.TestCase):
 
 	def test_get_prime_input_less_than_1_returns_none(self):
 		result = get_prime(1)
-		self.assertEquals(result, None)
+		self.assertEqual(result, None)
 
 
 if __name__ == '__main__':
