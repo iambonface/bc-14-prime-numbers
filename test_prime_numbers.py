2d58eaf8e9ddc6cedfe4d0e0eb8e2d642141ef21
Bonface Mudiri <mudirib@gmail.com>
2017-02-08 22:29:29 +0300

    Update tests in test_prime_numbers.py

---
+++ test_prime_numbers.py
@@ -0,0 +1,26 @@
+from prime_numbers import get_prime
+import unittest
+
+class TestPrimeNumbers(unittest.TestCase):
+
+	def test_get_prime_input_7_is_prime(self):
+		self.assertTrue(get_prime(7))
+
+	def test_get_prime_input_13_returns_prime_list(self):
+		result = get_prime(13)
+		self.assertEquals(result, [2,3,5,7,11,13])
+
+	def test_get_prime_input_str_raises_typeerror(self):
+		self.assertRaises(TypeError, get_prime, "num")
+
+	def test_get_prime_output_has_2_as_first_element(self):
+		result = get_prime(7)
+		self.assertEquals(result[0], 2)
+
+	def test_get_prime_output_includes_input_if_input_is_prime(self):
+		result = get_prime(13)
+		self.assertEquals(result[-1], 13)
+
+
+if __name__ == '__main__':
+	unittest.main()
