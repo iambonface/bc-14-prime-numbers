84daf40bad380f46043a65a7c8c356a07f025aa5
Bonface Mudiri <mudirib@gmail.com>
2017-02-09 01:06:53 +0300

    Fix imports in test_prime_numbers.py

--- test_prime_numbers.py
+++ test_prime_numbers.py
@@ -1,34 +1,33 @@
-
-from prime_numbers import get_prime
+import prime_numbers
 import unittest
 
 class TestPrimeNumbers(unittest.TestCase):
 
 	def test_get_prime_input_7_is_prime(self):
-		self.assertTrue(get_prime(7))
+		self.assertTrue(prime_numbers.get_prime(7))
 
 	def test_get_prime_input_13_returns_prime_list(self):
-		result = get_prime(13)
+		result = prime_numbers.get_prime(13)
 		self.assertEqual(result, [2,3,5,7,11,13])
 
 	def test_get_prime_input_str_raises_typeerror(self):
-		self.assertRaises(TypeError, get_prime, "num")
+		self.assertRaises(TypeError, prime_numbers.get_prime, "num")
 
 	def test_get_prime_output_has_2_as_first_element(self):
-		result = get_prime(7)
+		result = prime_numbers.get_prime(7)
 		self.assertEqual(result[0], 2)
 
 	def test_get_prime_output_includes_input_if_input_is_prime(self):
-		result = get_prime(13)
+		result = prime_numbers.get_prime(13)
 		self.assertEqual(result[-1], 13)
 
 	def test_get_prime_output_has_no_even_number_except_2(self):
-		result = get_prime(13)
+		result = prime_numbers.get_prime(13)
 		for i in result[1:]:
 			self.assertTrue(i % 2)
 
 	def test_get_prime_input_less_than_1_returns_none(self):
-		result = get_prime(1)
+		result = prime_numbers.get_prime(1)
 		self.assertEqual(result, None)
 
 
