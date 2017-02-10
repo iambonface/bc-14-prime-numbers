aec8f14cf75c1576c1542c3846c7f2b28f674481
Bonface Mudiri <mudirib@gmail.com>
2017-02-10 07:41:55 +0300

    Fix cyclomatic complexity in get_prime()

--- prime_numbers.py
+++ prime_numbers.py
@@ -12,15 +12,13 @@ Exceptions:
     Raise TypeError if input n is of any other type other than int
 """
 def get_prime(n):
-
-	if isinstance(n, int):
-		if n >= 2:
-			target_prime = range(2, n+1)
-			for i in range(2,8):
-				target_prime = [x for x in target_prime if x == i or x % i ]
-			return target_prime
-		else:
-			return None
-
-	else:
+	if not isinstance(n, int):
 		raise TypeError("Invalid input {}".format(type(n)))
+	elif n >= 2:
+		target_prime = range(2, n+1)
+		for i in range(2,8):
+			target_prime = [x for x in target_prime if x == i or x % i ]
+		return target_prime
+	else:
+		return None
+		
